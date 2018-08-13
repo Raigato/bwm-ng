e539572ba8397a22cf9bccc03a0bc4bba1e8669d
Gabriel Raymondou <gabrielraymondou@MacBook-Pro-de-Gabriel.local>
2018-08-13 19:48:40 +0200

    Adds pre-deploy setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-const config = require('./config/dev')
+const config = require('./config')
+const path = require('path')
 
 const FakeDb = require('./fake-db')
 const rentalRoutes = require('./routes/rentals')
@@ -9,8 +10,10 @@ const userRoutes = require('./routes/users')
 const bookingRoutes = require('./routes/bookings')
 
 mongoose.connect(config.DB_URI, { useNewUrlParser: true }).then(() => {
-  const fakeDb = new FakeDb()
-  //fakeDb.seedDb()
+  if (process.env.NODE_ENV !== 'production') {
+    const fakeDb = new FakeDb()
+    //fakeDb.seedDb()
+  }
 })
 
 const app = express()
@@ -21,8 +24,17 @@ app.use('/api/v1/rentals', rentalRoutes)
 app.use('/api/v1/users', userRoutes)
 app.use('/api/v1/bookings', bookingRoutes)
 
+if (process.env.NODE_ENV === 'production') {
+  const appPath = path.join(__dirname, '..', 'dist')
+  app.use(express.static(appPath))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(appPath, 'index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 3001
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log('App is running!')
 })
